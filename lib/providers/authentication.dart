4d3859f66ecff31b4430f8baac7c21baf0d26adb
yujinkim1 <yujin7621@me.com>
2023-01-10 12:02:13 +0900

    add: Categorizing serialization model by permission

---
+++ lib/providers/authentication.dart
@@ -0,0 +1,19 @@
+//MARK: LOGIN DATA MODEL
+class Authentication {
+  String? memberId;
+  String? password;
+
+  Authentication({this.memberId, this.password});
+
+  Authentication.fromJson(Map<String, dynamic> json) {
+    memberId = json["memberId"];
+    password = json["password"];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['memberId'] = memberId;
+    data['password'] = password;
+    return data;
+  }
+}

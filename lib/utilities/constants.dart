c6f924794962b1de51f7d2d9370e7c13eb951841
yujinkim1 <yujin7621@me.com>
2023-01-14 00:26:45 +0900

    add: Used service urls

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,6 @@
+//MARK: CONSTANTS VARIABLES
+class Constants {
+  static const LOGIN_URL = "http://waffle2.duckdns.org/main";
+  static const ADMIN_PATH = "http://waffle2.duckdns.org/admin";
+  static const MEMBER_PATH = "http://waffle2.duckdns.org/member";
+}
